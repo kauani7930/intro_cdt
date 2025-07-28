1fd3330a7f3671332beeb79dc0eafc01b9cb3056
kauani em vocacao <kauanilopess70@gmail.com>
2025-07-28 14:37:39 -0300

    Add files via upload

---
+++ nome_usuario.py
@@ -0,0 +1,8 @@
+nome_usuario = input("Olá! poderia me dizer qual e seu nome, por favor?")
+
+print ("___")
+print (f"O tipo de informações que vocês digitou é: {type(nome_usuario)}")
+print ("___")
+
+mensagem_personalizada =k
+kf"Que bom ter você aqui, {nome_usuario}! Seja muito bem-vindo(a) ao nosso programa!"
